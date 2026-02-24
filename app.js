ac93e6974024d0fff24387a2ecaa2f25020b006e
Gabriel Jeronimo <gabrieljeronimo.software@gmail.com>
2026-02-24 17:11:29 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+// O correto e usar a chave no backend
+const key = '835e0be4412628b5f3fb1d44d9fa9645';
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
