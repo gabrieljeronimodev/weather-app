dda2ce93a1080e75a1c9eab9b89fe9d843856257
Gabriel Jeronimo <gabrieljeronimo.software@gmail.com>
2026-02-24 17:41:39 -0300

    Add event listener in form

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // O correto e usar a chave no backend
 const key = '835e0be4412628b5f3fb1d44d9fa9645';
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
-const input = document.getElementById('input').value;
+const inputValue = document.getElementById('input').value;
 const form = document.getElementById('search-wrapper');
 const iconDisplay = document.getElementById('icon-display');
 const tempDisplay = document.getElementById('icon-display');
 const cityDisplay = document.getElementById('city-display');
 const humidDisplay = document.getElementById('humid-display');
 const windDisplay = document.getElementById('wind-display');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if(inputValue) {
+
+    }
+    else {
+        
+    }
+});
